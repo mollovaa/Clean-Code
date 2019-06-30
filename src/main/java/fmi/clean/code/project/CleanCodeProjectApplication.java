d2ddcc6adf8ccb83df0e84ecdeb91a1d1898ab44
Emel Mollova <emel.mollova@upnetix.com>
2019-06-30 14:44:54 +0300

    initial commit

---
+++ src/main/java/fmi/clean/code/project/CleanCodeProjectApplication.java
@@ -0,0 +1,13 @@
+package fmi.clean.code.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CleanCodeProjectApplication.class, args);
+  }
+
+}
